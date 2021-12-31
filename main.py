c88743afafb2b3272652f1928a354bed8d3e4d22
N1NJA <89378858+newbiepiee@users.noreply.github.com>
2021-12-31 13:57:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import requests
 import time
 
 def getWeather(canvas):
-    city = textfield.get()
-    api = "http://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=bafaed60ef6d12b70d3641c95406386a"
-    json_data = requests.get(api).json()
-    condition = json_data['weather'][0]['main']
-    temp = int(json_data['main']['temp'] - 273.15)
-    min_temp = int(json_data['main']['temp_min'] - 273.15)
-    max_temp = int(json_data['main']['temp_max'] - 273.15)
-    pressure = json_data['main']['pressure']
-    humidity = json_data['main']['humidity']
-    wind = json_data['wind']['speed']
-    sunrise = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunrise'] - 21600))
-    sunset = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunset'] - 21600))
+	city = textfield.get()
+	api = "http://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=bafaed60ef6d12b70d3641c95406386a"
+	r = requests.get(api)
+	if 'city not found' in r.text:
+		label1.config(text = "Location not found")
+		label2.config(text = "Try again")
+	else:
+		json_data = requests.get(api).json()
+		condition = json_data['weather'][0]['main']
+		temp = int(json_data['main']['temp'] - 273.15)
+		min_temp = int(json_data['main']['temp_min'] - 273.15)
+		max_temp = int(json_data['main']['temp_max'] - 273.15)
+		pressure = json_data['main']['pressure']
+		humidity = json_data['main']['humidity']
+		wind = json_data['wind']['speed']
+		sunrise = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunrise'] - 21600))
+		sunset = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunset'] - 21600))
 
-    final_info = condition + "\n" + str(temp) + "°C"
-    final_data = "\n" + "Max Temp: " + str(max_temp) + "\n" + "Min Temp: " + str(min_temp) + "\n" + "Pressure: " + str(pressure) + "\n" + "Humidity: " + str(humidity) + "\n" + "Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
-    label1.config(text = final_info)
-    label2.config(text = final_data)
-
-
+		final_info = condition + "\n" + str(temp) + "°C"
+		final_data = "\n" + "Max Temp: " + str(max_temp) + "\n" + "Min Temp: " + str(min_temp) + "\n" + "Pressure: " + str(pressure) + "\n" + "Humidity: " + str(humidity) + "\n" + "Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
+		label1.config(text = final_info)
+		label2.config(text = final_data)
 
 canvas = tk.Tk()
 canvas.geometry("600x500")
